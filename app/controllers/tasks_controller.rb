bee21fd03db1e99cf0c42727a31f9a95ae46042a
Michel Plainchamp <michel.plainchamp@gmail.com>
2017-05-23 17:15:27 +0200

    done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(name: params["task"]["name"], description: params["task"]["description"], check_done: false)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
+end
